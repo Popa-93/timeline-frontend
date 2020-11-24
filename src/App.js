e952bb3f59d04ccd977e1f3a4e3bfe014f961625
devNULLrebooted <moafred@gmail.com>
2020-11-24 15:02:46 +0100

    Replace json server 'stubapi' by  REST API server

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import './App.css';
+import "./App.css";
 
-import ErrorBoundary from './ErrorBoundary'
-import SkillSelector from './SkillSelector'
-import Timeline from './Timeline'
+import ErrorBoundary from "./ErrorBoundary";
+import SkillSelector from "./SkillSelector";
+import Timeline from "./Timeline";
 
 function App() {
   const [filter, setFilter] = useState([]);
   const [activities, setActivities] = useState([]);
 
-  useEffect( () => {
-    fetch(`${process.env.REACT_APP_BASE_URL}/activities`)
-      .then(res => res.json())
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/api/activities`)
+      .then((res) => res.json())
       .then(
-        (results) => { 
-          setFilter(results.map( (activity) => activity.id ));
-          setActivities(results);
-          },
+        (res) => {
+          setFilter(res.results.map((activity) => activity.id));
+          setActivities(res.results);
+        },
         (error) => {
-          console.log("Error during filter initialization fetch :", error);
-          })
-      //TODO add error processing and !!!! timeout handling !!!!
+          console.log("Error during activities initialization fetch :", error);
+        }
+      );
+    //TODO add error processing and !!!! timeout handling !!!!
   }, []); //Specify no dependency to ensure there this hook only run once at initialization
 
   return (
-      <>
+    <>
       <ErrorBoundary>
-        <SkillSelector activities={activities}
+        <SkillSelector
+          activities={activities}
           filter={filter}
-          setFilter={setFilter} >
-        </SkillSelector>
+          setFilter={setFilter}
+        ></SkillSelector>
       </ErrorBoundary>
       <ErrorBoundary>
-        <Timeline filter={filter}/>
+        <Timeline filter={filter} />
       </ErrorBoundary>
-      </>
-      
-  
+    </>
   );
 }
 
--- src/Timeline.js
+++ src/Timeline.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import Avatar from '@material-ui/core/Avatar';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import Avatar from "@material-ui/core/Avatar";
 
 const useStyles = makeStyles((theme) => ({
   timeline: {
@@ -13,50 +15,61 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Timeline (props) {
+function Timeline(props) {
   const classes = useStyles();
 
   const [records, setRecords] = useState([]);
 
-  useEffect( () => {
-    fetch(`${process.env.REACT_APP_BASE_URL}/records`)
-      .then(res => res.json())
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND_BASE_URL}/api/records`)
+      .then((res) => res.json())
       .then(
-        (results) => { 
-          setRecords(results);
-          },
+        (res) => {
+          setRecords(res.results);
+        },
         (error) => {
           console.log("Error during records initialization fetch :", error);
-          })
-      //TODO add error processing and !!!! timeout handling !!!!
+        }
+      );
+    //TODO add error processing and !!!! timeout handling !!!!
   }, []);
 
   return (
-    <VerticalTimeline className= {classes.timeline}>
+    <VerticalTimeline className={classes.timeline}>
       {records
-        .filter((record) => 
-          record.activities.filter( (activity) => props.filter.includes(activity) ).length
-          )
+        .filter(
+          (record) =>
+            record.activities.filter((activity) =>
+              props.filter.includes(activity)
+            ).length
+        )
         .map((record) => {
-        return (
-        <VerticalTimelineElement
-          key = {record.id}
-          className="vertical-timeline-element--school"
-          date={record.date}
-          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-          //TODO use common avatar in view and filter
-          icon={<Avatar
-            src={`${process.env.PUBLIC_URL}/images/skills/${record.activitiesName}.png`}
-            alt={record.activitiesName}
-            />}
-          >
-            <h3 className="vertical-timeline-element-title">{record.title}</h3>
-            <h4 className="vertical-timeline-element-subtitle">{record.subtitle}</h4>
-            <p>{record.desc} </p>
-        </VerticalTimelineElement>);
-    })}
-
+          return (
+            <VerticalTimelineElement
+              key={record.id}
+              className="vertical-timeline-element--school"
+              date={record.date}
+              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              //TODO use common avatar in view and filter
+              icon={
+                <Avatar
+                  src={`${process.env.PUBLIC_URL}/images/skills/${record.activitiesName}.png`}
+                  alt={record.activitiesName}
+                />
+              }
+            >
+              <h3 className="vertical-timeline-element-title">
+                {record.title}
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                {record.subtitle}
+              </h4>
+              <p>{record.desc} </p>
+            </VerticalTimelineElement>
+          );
+        })}
     </VerticalTimeline>
-);}
+  );
+}
 
 export default Timeline;
